bd3374a1390b8272448a62c7136ec706305b7e07
Aryan <adusharma22@gmail.com>
2022-03-08 23:59:39 +0530

    removed .txt wordlist because no use

--- words.py
+++ words.py
@@ -1,9 +1,15 @@
-import re 
+import re
+import requests 
+
 # wordlist is html downloaded from https://eslforums.com/5-letter-words/ downloaded because requests didn't work out 
 with open(r"C:\Users\adush\Downloads\wordlist.html", "r") as f:
-    text = f.read()
-    words = re.findall(r"<li>(\S{5})", text)
+    text = requests.get("https://www.thefreedictionary.com/5-letter-words.htm").text
+    words = re.findall(r'<li data-f=15><a href="(\S{5})', text)
 
-with open("words.txt", "w+") as file:
-    for word in words:
-        file.write(word + "\n")
+# with open("words.txt", "w+") as file:
+#     for word in words:
+#         file.write(word + "\n")
+
+with open('words.ts', 'w') as js:
+    wordlist = "[" + ", ".join(f"'{word}'" for word in words) + "]"
+    js.write(wordlist)
